44be67c740403f98a31995ea0dfe5e5d010bc262
Alan Samycia <110893287+SleepyAlan@users.noreply.github.com>
2024-12-04 19:29:39 -0700

    Completed Rental/Queue Page, removed button behavior for accessing queue from multiple screens

---
+++ movieRental/skeleton/CustomerOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace movieRental
+{
+    class CustomerOrder
+    {
+        public string MovieName { get; set; }
+        public int? CustomerRating { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+
+    }
+}
